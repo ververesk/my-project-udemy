53ac241337c8201ff9556efd342dfa864f332167
Veronika Hryharovich <weronnika@list.ru>
2021-09-23 15:17:38 +0300

    Started to study aop

---
+++ src/main/java/org/spring/example/aop/aspects/LoggingAspect.java
@@ -0,0 +1,16 @@
+package org.spring.example.aop.aspects;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LoggingAspect {
+
+    @Before("execution(public void getBook())")
+    public void beforeGetBookAdvice() {
+        System.out.println("beforeGetBookAdvice: попытка получить книгу");
+    }
+}
